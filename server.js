7f010c769ec7263ac9ac0fbda890d519620aeb5d
Taqiyya Ghazi <taqiyyaghazi@gmail.com>
2022-03-17 22:24:20 +0700

    Menambah API Hapus Data

--- server.js
+++ server.js
@@ -197,32 +197,59 @@ const requestListener = function (req, res) {
               };
               res.end(JSON.stringify(response_payload));
             }
-
-            // var list_id = data["id"].sort();
-            // var last_id_post = list_id[list_id.length - 1];
-            // var new_id = last_id_post + 1;
-
-            // var new_post = {
-            //   id_post: new_id,
-            //   judul: judul,
-            //   tanggal: tanggal,
-            //   kategori: kategori,
-            //   deskripsi: deskripsi,
-            // };
-
-            // data["post"].push(new_post);
-            // data["id"].push(new_id);
-            // data = JSON.stringify(data);
-            // fs.writeFile("data.json", data, (err) => {
-            //   if (err) throw err;
-            //   var response_payload = {
-            //     deskripsi: "Berhasil Menambah data",
-            //     result: new_post,
-            //   };
-            //   res.end(JSON.stringify(response_payload));
-            // });
           });
         });
+      break;
+
+    //   API untuk Menghapus Data Postingan
+    case "/delete_data_post":
+      console.log("HIT API Delete Post");
+      const parameters_delete = url.parse(req.url, true).query;
+
+      fs.readFile("./data.json", "utf8", (err, jsonString) => {
+        if (err) {
+          console.log("File read failed:", err);
+          return;
+        }
+        var data = JSON.parse(jsonString);
+        if ("id" in parameters_delete) {
+          data["post"].forEach(function (value, index, array) {
+            //   Mencari id_post yang sama dengan parameter id
+            if (parameters_delete["id"] == value["id_post"]) {
+              var data_delete = data["post"];
+              var id_delete = data["id"];
+
+              data_delete = data_delete.filter((item) => item !== value);
+              id_delete = id_delete.filter((item) => item !== value["id_post"]);
+
+              data["id"] = id_delete;
+              data["post"] = data_delete;
+              fs.writeFile("data.json", JSON.stringify(data), (err) => {
+                if (err) throw err;
+                var response_payload = {
+                  deskripsi: "Berhasil Menghapus data",
+                  result: data,
+                };
+                res.end(JSON.stringify(response_payload));
+              });
+            }
+          });
+
+          var response_payload = {
+            deskripsi: "Data Tidak Ditemukan",
+            result: "-",
+          };
+          res.end(JSON.stringify(response_payload));
+        } else {
+          // Respon ketika parameter id belum ditentukan
+          console.log("ID belum ditentukan");
+          var response_payload = {
+            deskripsi: "Parameter id belum ditentukan",
+            result: "-",
+          };
+          res.end(JSON.stringify(response_payload));
+        }
+      });
       break;
   }
 };
